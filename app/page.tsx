e916db26beac87351fe70dc155632292caee86f1
rakibbateq <mdrakibulislaamm@gmail.com>
2023-07-07 18:10:51 +0600

    new device

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className='text-5xl text-violet-600'>Hellow its Shahin</p>
+      <p className='text-5xl text-violet-600'>Hellow its Shahin bro</p>
     </main>
   )
 }
